d36f8f0590b1756625a374e38c206f72b2fe46ff
HAMINZZZZ <hh32@students.calvin.edu>
2018-09-20 16:24:05 -0400

    combined folders

--- lab01/lab01.js
+++ lab01/lab01.js
@@ -1,5 +1,5 @@
 /*
-Lab02 started by Hamin Hong on Sept. 12th, 2018
+Lab01 started by Hamin Hong on Sept. 12th, 2018
 CS 336
 Professor Vander Liden
 */
@@ -36,12 +36,10 @@ function birthday(Name, day, month, year, age){
 	
 }
 
-console.log( "user1 is an instanceof person?",user1 instanceof person);
-
 birthday.prototype = Object.create(person.prototype);
 birthday.prototype.ages = function() {
 
-    var today = new Date();
+	var today = new Date();
     var birthyear = new Date(this.year);
     var birthmonth = new Date(this.month);
     var birthdate = new Date(this.date);
@@ -53,13 +51,10 @@ birthday.prototype.ages = function() {
     
 }
 
-var user2 = new birthday("Jungmin", 14, 8, 1995, "unknown");
+var user2 = new birthday("Jungmin", 1, 9, 1995, "unknown");
 console.log(user2);
 user2.ages();
 console.log(user2);
-
-console.log( "user2 is an instanceof person?",user2 instanceof person);
-
 
 //List of friends! & the method for adding onto the list
 
@@ -87,11 +82,8 @@ user3.add("Bill");
 console.log(user3);
 
 
-console.log( "user3 is an instanceof person?",user3 instanceof person);
-
-
-
-//encapsulation, inheritance, and polymorphism enables such use of data and management.
+//encapsulation, inheritance, and polymorphism enables 
+//protection and use of data and management.
 
 person.prototype.introduction = function(){
 	console.log("Hello! my name is", this.Name);
@@ -99,33 +91,4 @@ person.prototype.introduction = function(){
 
 var user4 = new person("Frog_prince");
 user4.introduction();
-
-console.log( "user4 is an instanceof person?",user4 instanceof person);
-
-
-function student(Name, subject){
-	person.call(this,Name);
-	this.subject = subject;
-}
-
-student.prototype = Object.create(person.prototype);
-student.prototype.subjects = function(subjectC) {
-	this.subject = subjectC;
-}
-
-var user5 = new student("Jacob", "Undeclared");
-console.log(user5);
-user5.subjects("Squirrel Psychology");
-console.log(user5);
-user5.rename("Watch");
-console.log(user5);
-user5.introduction();
-
-console.log( "user5 is an instanceof person?",user5 instanceof person);
-console.log( "user5 is an instanceof birthday?",user5 instanceof birthday);
-console.log( "user5 is an instanceof friends?",user5 instanceof friends);
-console.log( "user5 is an instanceof student?",user5 instanceof student);
-//The instanceof function shows that function student uses inheritance
-//and polymorphism to get access to the parent function person,
-//but not those of birthday and friends(encapsulation)
 
